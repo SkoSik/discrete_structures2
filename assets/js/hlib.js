204e88d5aa8bf01bde9c9e19dd801702632df478
SkoSik <skos2002d@gmail.com>
2022-05-07 23:03:44 +0800

    закинул

--- assets/js/hlib.js
+++ assets/js/hlib.js
@@ -1,7 +1,3 @@
-$(document).ready(function () {
-    console.log("кирич лох");
-});
-
 class Graph {
     verts;
     points;
@@ -10,10 +6,11 @@ class Graph {
         this.verts = [];
         this.points = [];
     }
+
     add(a, b) {
-        if(!this.verts[a]) this.verts[a] = [b];
+        if (!this.verts[a]) this.verts[a] = [b];
         else this.verts[a].push(b);
-        if(!this.verts[b]) this.verts[b] = [a];
+        if (!this.verts[b]) this.verts[b] = [a];
         else this.verts[a].push(b);
     }
 }
@@ -31,21 +28,33 @@ class Point {
 
 }
 
-let used = [];
 let graph = new Graph();
-graph.add(1,2);
-graph.add(1,3);
-graph.add(1,4);
-graph.add(2,3);
+let used = [];
+$(document).ready(function () {
 
-console.log(graph.verts);
-dfs(1);
-console.log(used);
+    $('#dfs').on('click', function () {
+        dfs(1);
+        console.log(used);
+        graph = new Graph();
+        used = [];
+    });
 
+    $('#add').on('click', function () {
+        let a = parseInt($('#va').val(), 10);
+        let b = parseInt($('#vb').val(), 10);
+
+        if (!a || !b) return;
+
+        graph.add(a, b);
+        graph.add(b, a);
+
+        $('#input').val($('#input').val() + a + " " + b + "\n");
+    });
+});
 
 function dfs(v) {
     used.push(v);
-    graph.verts[v].forEach(function(el){
-        if(!used.includes(el)) dfs(el);
+    graph.verts[v].forEach(function (el) {
+        if (!used.includes(el)) dfs(el);
     });
 }
